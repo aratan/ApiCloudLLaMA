57dac345b051365c89965d932873e2ba59244c86
Victor Arbiol <victor.arbiol@gmail.com>
2023-05-16 17:47:51 +0200

    add jobs
    Queries are now working and job retrieval is working as well.

--- api.go
+++ api.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"sync"
 	"time"
@@ -67,6 +68,37 @@ func generateJobID() string {
 	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
 
+func handleJobs(w http.ResponseWriter, r *http.Request) {
+	// Obtener una lista de todos los trabajos en la cola
+	jobManager := NewJobManager()
+	jobs := make([]*Job, 0)
+	jobManager.RLock()
+	for _, job := range jobManager.Jobs {
+		jobs = append(jobs, job)
+	}
+	jobManager.RUnlock()
+
+	// Crear una estructura que contenga los datos a devolver en formato JSON
+	response := struct {
+		Jobs []*Job `json:"jobs"`
+	}{
+		Jobs: jobs,
+	}
+
+	// Codificar la estructura de respuesta en formato JSON
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Establecer la cabecera de la respuesta HTTP para indicar que se devuelve JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Escribir la respuesta JSON en el cuerpo de la respuesta HTTP
+	w.Write(jsonResponse)
+}
+
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// Obtener la frase de la solicitud HTTP
 	phrase := r.FormValue("phrase")
@@ -78,7 +110,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// Ejecutar la aplicación de terminal "llama" en segundo plano
 	go func(job *Job) {
 		cmd := exec.Command("./main", "-m", "./WizardLM-7B-uncensored.ggml.q4_0.bin", "-p", job.Phrase, "-n", "512")
-        fmt.Println(cmd)
+		fmt.Println(cmd)
 		// Obtener la salida de la aplicación
 		output, err := cmd.Output()
 		if err != nil {
@@ -107,50 +139,68 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleResult(w http.ResponseWriter, r *http.Request) {
-	// Obtener la ID del trabajo de la solicitud HTTP
-	jobID := r.FormValue("jobID")
+	// Obtener el valor del parámetro jobID de la solicitud HTTP
+	jobID := r.URL.Query().Get("jobID")
 
-	// Obtener el trabajo correspondiente de la cola de trabajos
-	jobManager := NewJobManager()
-	job, ok := jobManager.GetJob(jobID)
-	if !ok {
-		http.Error(w, "Trabajo no encontrado", http.StatusNotFound)
-		return
+	// Comprobar si el archivo jobID.txt existe en el directorio actual
+	_, err := os.Stat(jobID + ".txt")
+	exists := !os.IsNotExist(err)
+
+	// Obtener la dirección IP del cliente que realiza la solicitud HTTP
+	ip := r.RemoteAddr
+
+	// Crear una estructura que contenga los datos a devolver en formato JSON
+	response := struct {
+		JobID  string    `json:"jobID"`
+		Content string   `json:"content,omitempty"`
+		Exists bool      `json:"exists"`
+		Date   time.Time `json:"date"`
+		IP     string    `json:"ip"`
+	}{
+		JobID:  jobID,
+		Exists: exists,
+		Date:   time.Now(),
+		IP:     ip,
 	}
 
-	// Leer la salida de la aplicación desde el archivo
-	output, err := ioutil.ReadFile(fmt.Sprintf("%s.txt", job.ID))
+	if exists {
+		// Leer el contenido del archivo jobID.txt
+		content, err := ioutil.ReadFile(fmt.Sprintf("%s.txt", jobID))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		response.Content = string(content)
+	}
+
+	// Codificar la estructura de respuesta en formato JSON
+	jsonResponse, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Eliminar el trabajo de la cola de trabajos
-	jobManager.DeleteJob(job.ID)
-
-	// Crear una estructura de resultado con la salida de la aplicación
-	result := &Result{Output: string(output)}
-
-	// Codificar la estructura de resultado en JSON y enviarla como respuesta HTTP
-	jsonResult, err := json.Marshal(result)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
+	// Establecer la cabecera de la respuesta HTTP para indicar que se devuelve JSON
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonResult)
+
+	// Escribir la respuesta JSON en el cuerpo de la respuesta HTTP
+	w.Write(jsonResponse)
 }
 
 func main() {
-    fmt.Println("service enabled localhost:8080")
-    fmt.Println("http://localhost:8080/llama?phrase=hola")
-    fmt.Println("http://localhost:8080/result?jobID=1684222811777450241")
+	fmt.Println("service enabled localhost:8080")
+	fmt.Println("http://localhost:8080/llama?phrase=hola")
+	fmt.Println("http://localhost:8080/result?jobID=1684222811777450241")
+	fmt.Println("http://localhost:8080/jobs")
+
 	// Asociar la función de manejo de solicitudes a la ruta /llama
 	http.HandleFunc("/llama", handleRequest)
 
 	// Asociar la función de manejo de solicitudes a la ruta /result
 	http.HandleFunc("/result", handleResult)
+
+	//ver las colas de trabajo
+	http.HandleFunc("/jobs", handleJobs)
 
 	// Iniciar el servidor HTTP en el puerto 8080
 	log.Fatal(http.ListenAndServe(":8080", nil))
